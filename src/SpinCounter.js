ccfb4ff0ce7f629a23bb2a7f8e24a55308da5fb5
jot6000 <joemercer50@googlemail.com>
2021-11-29 11:08:42 +0000

    Made slightly easier

--- src/SpinCounter.js
+++ src/SpinCounter.js
@@ -3,10 +3,8 @@ import { useEffect, useState } from "react"
 export default function spinCounter(){
     const [spin,setspin] = useState(0)
     useEffect(() => {
-        while(true){
-            setTimeout(function(){ setspin(spin+1) }, 500)
-        }
-      }, []);
+        setTimeout(function(){ setspin(spin+1) }, 500)
+      }, [spin]);
     return(
         <div>
             I have spun {spin} times
